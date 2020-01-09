ebf1cf7f9c8418a81352ccd85a562c2c7910706d
Archanciel <jp.schnyder@gmail.com>
2020-01-09 17:12:35 +0100

    Elimated full curve and subplot

--- chap1_8_adding_piecewise_spline_standard_form_best_gui.m
+++ chap1_8_adding_piecewise_spline_standard_form_best_gui.m
@@ -1,7 +1,4 @@
 clear all
-DO_SUBPLOT = 2 %if set to 1, only plots the full curves graph
-               %if set to 2, only plots the partial curves graph
-               %if set to 3, plots both graphs
 
 p1 = [0 1]
 p2 = [2 2]
@@ -63,52 +60,10 @@ vpa(y_C)
 close all
 figure
 
-%plotting full initial piecewise curves
-if DO_SUBPLOT == 3 %if set to 1, only plots the full curves graph
-                   %if set to 2, only plots the partial curves graph
-                   %if set to 3, plots both graphs
-    subplot(121)
-end
-
 xx_lim = [p1(1,1) - 1 p8(1,1) + 1]
 xx_all = linspace(xx_lim(1,1),xx_lim(1,2));
 
-if DO_SUBPLOT ~= 2 %if set to 1, only plots the full curves graph
-                   %if set to 2, only plots the partial curves graph
-                   %if set to 3, plots both graphs
-    clear yy_A
-    clear yy_B
-    clear yy_C
-
-    yy_A = subs(y_A, x, xx_all);
-    plot(xx_all, yy_A, 'b');
-    hold on
-    yy_B = subs(y_B, x, xx_all);
-    plot(xx_all, yy_B, 'r');
-    yy_C = subs(y_C, x, xx_all);
-    plot(xx_all, yy_C, 'm');
-
-    scatter(Pn(:,1),Pn(:,2),50,'filled')
-    text(Pn(1,1)+0.1, Pn(1,2)-0.1, 'P_1');
-    text(Pn(2,1)+0.1, Pn(2,2)-0.1, 'P_2');
-    text(Pn(3,1)+0.1, Pn(3,2)-0.1, 'P_3');
-    text(Pn(4,1)+0.1, Pn(4,2)-0.1, 'P_4');
-
-    xlabel('x')
-    ylabel('y')
-    title(["Piecewise spline + 1" "standard form -" "full curves"])
-
-    set(gca,'ylim',[-5 10],'xlim',[xx_lim(1,1) xx_lim(1,2)],'xtick',xx_lim(1,1):xx_lim(1,2),'ytick',-5:10)
-    opt.fontname = 'helvetica';
-    opt.fontsize = 8;
-
-    centeraxes(gca,opt);
-end
-
 %plotting partial initial piecewise curves
-if DO_SUBPLOT == 3
-    subplot(122)
-end
 
 clear yy_A
 clear yy_B
@@ -117,7 +72,7 @@ clear yy_C
 xx_lim_A = [Pn(1,1) - 1 Pn(2,1)]
 xx_A = linspace(xx_lim(1,1),xx_lim_A(1,2));
 yy_A = subs(y_A, x, xx_A);
-plot(xx_A, yy_A, 'b');
+hplot = plot(xx_A, yy_A, 'b');
 
 hold on
 xx_lim_B = [Pn(2,1) Pn(3,1)]
@@ -190,36 +145,7 @@ vpa(y_E)
 fprintf("y_F")
 vpa(y_F)
 
-%plotting full added piecewise curves
-if DO_SUBPLOT == 3
-    subplot(121)
-end
-
-if DO_SUBPLOT ~= 2 %if set to 1, only plots the full curves graph
-                   %if set to 2, only plots the partial curves graph
-                   %if set to 3, plots both graphs
-    clear yy_D
-    clear yy_E
-    clear yy_F
-
-    yy_D = subs(y_D, x, xx_all);
-    plot(xx_all, yy_D, 'k');
-    yy_E = subs(y_E, x, xx_all);
-    plot(xx_all, yy_E, 'r');
-    yy_F = subs(y_F, x, xx_all);
-    plot(xx_all, yy_F, 'g');
-
-    scatter(Pn(:,1),Pn(:,2),50,'filled')
-    text(Pn(1,1)-0.3, Pn(1,2)-0.3, 'P_5');
-    text(Pn(2,1)+0.1, Pn(2,2)-0.1, 'P_6');
-    text(Pn(3,1)+0.1, Pn(3,2)-0.1, 'P_7');
-    text(Pn(4,1)+0.1, Pn(4,2)-0.1, 'P_8');
-end
-
 %plotting partial added piecewise curves
-if DO_SUBPLOT == 3
-    subplot(122)
-end
 
 clear yy_D
 clear yy_E
@@ -246,3 +172,85 @@ text(Pn(1,1)-0.3, Pn(1,2)-0.3, 'P_5');
 text(Pn(2,1)+0.1, Pn(2,2)-0.1, 'P_6');
 text(Pn(3,1)+0.1, Pn(3,2)-0.1, 'P_7');
 text(Pn(4,1)+0.1, Pn(4,2)-0.1, 'P_8');
+
+SLIDER_POS_X = 150
+MENU_POS_X = 70
+
+xMin = -10
+xMax = 10
+yMin = -10
+yMax = 10
+
+global line
+
+line{1} = [-8 5] % 2 points x coordinates
+line{2} = [-4 7] % 2 points y coordinates
+line{3} = 'p1'   % initial value of menu 
+    
+xSlider = uicontrol('style','slider','units','pixel','position',[SLIDER_POS_X 20 300 20],...
+    'sliderstep',[1/(xMax-xMin), 2/(xMax-xMin)],'max',xMax,'min',xMin, 'value',line{1}(1,1));
+addlistener(xSlider,'ContinuousValueChange',@(hObject, event) sliderPlot_x(hObject, event,hplot));
+line{4} = xSlider % required so that menu selection can update slider values
+uicontrol('style','text',...
+    'position',[SLIDER_POS_X - 10 30 10 10],'string', 'X');
+
+% slider controlling y coordinates
+ySlider = uicontrol('style','slider','units','pixel','position',[SLIDER_POS_X 0 300 20],...
+    'sliderstep',[1/(xMax-xMin), 2/(xMax-xMin)],'max',xMax,'min',xMin, 'value',line{2}(1,1));
+line{5} = ySlider
+addlistener(ySlider,'ContinuousValueChange',@(hObject, event) sliderPlot_y(hObject, event,hplot));
+uicontrol('style','text',...
+    'position',[SLIDER_POS_X - 10 10 10 10],'string', 'Y');
+
+% drop down menu to select which point is modified by the sliders 
+menu = uicontrol('Style','popupmenu',...
+    'position', [MENU_POS_X 20 60 20], 'string', {'p1','p2'});
+menu.Callback = @menuSelection;
+
+% callback functions
+
+function sliderPlot_x(hObject,event,hplot)
+    global line
+    n = get(hObject,'Value')
+    
+    if line{3} == 'p1'
+        line{1}(1,1) = n
+    else
+        line{1}(1,2) = n
+    end
+    
+    set(hplot,'xdata',line{1});
+    drawnow;
+end
+
+function sliderPlot_y(hObject,event,hplot)
+    global line
+    n = get(hObject,'Value')
+    
+    if line{3} == 'p1'
+        line{2}(1,1) = n
+    else
+        line{2}(1,2) = n
+    end
+    
+    set(hplot,'ydata',line{2});
+    drawnow;
+end
+
+function menuSelection(hObject,event)
+    global line
+    
+    val = get(hObject,'Value');
+    str = get(hObject,'String');
+    line{3} = str{val};
+    xSlider = line{4};
+    ySlider = line{5};
+
+    if line{3} == 'p1'
+        xSlider.Value = line{1}(1,1);
+        ySlider.Value = line{2}(1,1);
+    else
+        xSlider.Value = line{1}(1,2);
+        ySlider.Value = line{2}(1,2);
+    end
+end
