f821c59bf5499394cd23cbe419762163f40c7cdb
Archanciel <jp.schnyder@gmail.com>
2020-01-19 18:01:03 +0100

    using uiData in progress

---
+++ UIControlsData.m
@@ -0,0 +1,57 @@
+% {1} --> curve points x coordinates
+                           % {2} --> curve points y coordinates
+                           % {3} current menu selection value 
+                           % {4} x slider reference 
+                           % {5} y slider reference 
+                           % {6} x slider text value reference 
+                           % {7} y slider text value reference 
+                           % {8} --> NO LONGER USED !
+                           % {9} --> initial piecewise spline points_labels strings
+                           % {10} --> additional piecewise spline points_labels strings
+                           % {11} --> initial piecewise spline colors
+                           % {12} --> additional piecewise spline colors
+                           % {13} --> first piecewise spline line handles
+                           % {14} --> additional piecewise spline line handles
+                           % {15} --> initial piecewise spline point label handles
+                           % {16} --> additional piecewise spline point label handles
+                           % {17} --> initial piecewise spline scattered point handles
+                           % {18} --> additional piecewise spline scattered point handles
+classdef UIControlsData < handle
+    properties
+        % constants
+        SCATTER_POINT_SIZE = 15;
+        XY_SLIDER_STEP = 0.1;
+        
+        pointMenuCurrentSelection;
+        xSliderHandle;
+        ySliderHandle;
+        xSliderTextValueHandle;
+        ySliderTextValueHandle;
+    end
+    methods
+        function obj = UIControlsData()
+            obj.pointMenuCurrentSelection = 'P1';
+        end
+        
+        % using property setter (similar to python @property annotaton)
+        function obj = set.pointMenuCurrentSelection(obj, value)
+             obj.pointMenuCurrentSelection = value;
+        end
+        
+        function obj = set.xSliderHandle(obj, value)
+             obj.xSliderHandle = value;
+        end
+        
+        function obj = set.ySliderHandle(obj, value)
+             obj.ySliderHandle = value;
+        end
+        
+        function obj = set.xSliderTextValueHandle(obj, value)
+             obj.xSliderTextValueHandle = value;
+        end
+        
+        function obj = set.ySliderTextValueHandle(obj, value)
+             obj.ySliderTextValueHandle = value;
+        end
+    end
+end
--- chap1_8_adding_piecewise_spline_standard_form_best_gui.m
+++ chap1_8_adding_piecewise_spline_standard_form_best_gui.m
@@ -1,11 +1,5 @@
 clear all
 
-global SCATTER_POINT_SIZE;
-SCATTER_POINT_SIZE = 15;
-
-global XY_SLIDER_STEP;
-XY_SLIDER_STEP = 0.1;
-
 global global_splines_data;% {1} --> curve points x coordinates
                            % {2} --> curve points y coordinates
                            % {3} --> current menu selection value 
@@ -24,6 +18,12 @@ global global_splines_data;% {1} --> curve points x coordinates
                            % {16} --> additional piecewise spline point label handles
                            % {17} --> initial piecewise spline scattered point handles
                            % {18} --> additional piecewise spline scattered point handles
+
+global uiData; % must be declared as global variable since this is the unique
+               % the popupmenu callback function can acess to it ! In other
+               % calbacks, uiData is passed as argument to the function.
+uiData = UIControlsData();
+
 
 points_labels{1} = 'P_1';
 points_labels{2} = 'P_2';
@@ -70,31 +70,30 @@ P_5_8 = [p5;p6;p7;p8];
 % initializing the global spline_data variable (for use by ui callback functions)
 global_splines_data{1} = [P_1_4(:,1)' P_5_8(:,1)']; % curve points x coordinates
 global_splines_data{2} = [P_1_4(:,2)' P_5_8(:,2)']; % curve points y coordinates
-global_splines_data{3} = 'P1';   % initial value of menu 
+uiData.pointMenuCurrentSelection = 'P1';   % initial value of menu 
 
 yFuncCellArray = computeFirstPiecewiseSpline();
-plotFirstPiecewiseSpline(yFuncCellArray);
+plotFirstPiecewiseSpline(yFuncCellArray, uiData);
 
 yFuncCellArray = computeAdditionalPiecewiseSpline();
-plotAdditionalPiecewiseSpline(yFuncCellArray);
-addUI();
+plotAdditionalPiecewiseSpline(yFuncCellArray, uiData);
+addUI(uiData);
 
-function addUI()
+function addUI(uiData)
     % Adds UI controls
     
-    global XY_SLIDER_STEP;
     global global_splines_data;
-
+    
     SLIDER_POS_X = 150;
     MENU_POS_X = 70;
 
-    [xSliderMin xSliderMax] = getMinMaxX(1);
+    [xSliderMin xSliderMax] = getMinMaxX(1, uiData);
     yMin = -10;
     yMax = 10;
 
     xSlider = uicontrol('style','slider','units','pixel','position',[SLIDER_POS_X 20 300 20],...
-        'sliderstep',[XY_SLIDER_STEP/(xSliderMax-xSliderMin), XY_SLIDER_STEP * 5/(xSliderMax-xSliderMin)],'max',xSliderMax,'min',xSliderMin, 'value',global_splines_data{1}(1,1));
-    addlistener(xSlider,'ContinuousValueChange',@(hObject, event) sliderPlot_x(hObject, event));
+        'sliderstep',[uiData.XY_SLIDER_STEP/(xSliderMax-xSliderMin), uiData.XY_SLIDER_STEP * 5/(xSliderMax-xSliderMin)],'max',xSliderMax,'min',xSliderMin, 'value',global_splines_data{1}(1,1));
+    addlistener(xSlider,'ContinuousValueChange',@(hObject, event) sliderPlot_x(hObject, event, uiData));
     global_splines_data{4} = xSlider; % required so that menu selection can update the curve point impacted by the x slider 
     uicontrol('style','text',...
         'position',[SLIDER_POS_X - 10 30 10 10],'string', 'X');
@@ -104,23 +103,27 @@ function addUI()
 
     % slider controlling y coordinates
     ySlider = uicontrol('style','slider','units','pixel','position',[SLIDER_POS_X 0 300 20],...
-        'sliderstep',[XY_SLIDER_STEP/(yMax-yMin), XY_SLIDER_STEP * 5/(yMax-yMin)],'max',yMax,'min',yMin, 'value',global_splines_data{2}(1,1));
+        'sliderstep',[uiData.XY_SLIDER_STEP/(yMax-yMin), uiData.XY_SLIDER_STEP * 5/(yMax-yMin)],'max',yMax,'min',yMin, 'value',global_splines_data{2}(1,1));
     global_splines_data{5} = ySlider; % required so that menu selection can update the curve point impacted by the y slider 
-    addlistener(ySlider,'ContinuousValueChange',@(hObject, event) sliderPlot_y(hObject, event));
+    addlistener(ySlider,'ContinuousValueChange',@(hObject, event) sliderPlot_y(hObject, event, uiData));
     uicontrol('style','text',...
         'position',[SLIDER_POS_X - 10 10 10 10],'string', 'Y');
     yValueText = uicontrol('Style','text','Position',[SLIDER_POS_X + 302,5,20,15],...
                 'String',global_splines_data{2}(1,1), 'BackgroundColor', 'w');
     global_splines_data{7} = yValueText; % required so that the y slider can update its displayed value
 
-    % drop down menu to select which point is modified by the sliders 
+    % drop down menu to select which point is modified by the sliders
+    % since two separate piecewise spines are used, P4 and P5 can not be modified
+    % through the UI !
     menu = uicontrol('Style','popupmenu',...
-        'position', [MENU_POS_X 20 60 20], 'string', {'P1','P2','P3','P4','P5','P6','P7','P8'});
+        'position', [MENU_POS_X 20 60 20], 'string', {'P1','P2','P3','P6','P7','P8'});
+%    menu = uicontrol('Style','popupmenu',...
+%        'position', [MENU_POS_X 20 60 20], 'string', {'P1','P2','P3','P4','P5','P6','P7','P8'});
     menu.Callback = @menuSelection;
+    addlistener(menu,'ContinuousValueChange',@(hObject, event) sliderPlot_y(hObject, event));
 end
 
-function [minX maxX] = getMinMaxX(pointIndex)
-    global XY_SLIDER_STEP;
+function [minX maxX] = getMinMaxX(pointIndex, uiData)
     global global_splines_data;
     
     [xAxisMin, xAxisMax] = getXAxisLimits();
@@ -129,14 +132,14 @@ function [minX maxX] = getMinMaxX(pointIndex)
     if pointIndex == 1
         minX = xAxisMin;
     else    
-        prevPointX = global_splines_data{1}(1, pointIndex - 1) + XY_SLIDER_STEP;
+        prevPointX = global_splines_data{1}(1, pointIndex - 1) + uiData.XY_SLIDER_STEP;
         minX = min(prevPointX, currentX);
     end
       
     if pointIndex == 8
         maxX = xAxisMax;
     else
-        nextPointX = global_splines_data{1}(1, pointIndex + 1) - XY_SLIDER_STEP;
+        nextPointX = global_splines_data{1}(1, pointIndex + 1) - uiData.XY_SLIDER_STEP;
         maxX = max(nextPointX, currentX);
     end
 end 
@@ -188,7 +191,7 @@ function yFuncCellArray = computeFirstPiecewiseSpline()
     yFuncCellArray{3} = y_C;
 end
 
-function plotFirstPiecewiseSpline(yFuncCellArray)
+function plotFirstPiecewiseSpline(yFuncCellArray, uiData)
     global global_splines_data;
 
     Pn = [global_splines_data{1}(1,1:4)' global_splines_data{2}(1,1:4)'];
@@ -208,7 +211,8 @@ function plotFirstPiecewiseSpline(yFuncCellArray)
     
     isAdditionalSpline = 0; % first point label will be shifted to avoid overwritting
                             % last point label of initial piecewise spline
-    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
+    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(uiData,...
+                                                              Pn,...
                                                               y_A,...
                                                               y_B,...
                                                               y_C,...
@@ -237,7 +241,8 @@ function [xAxisMin, xAxisMax] = getXAxisLimits()
     xAxisMax = global_splines_data{1}(1,8) + 1;
 end
 
-function plottedPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
+function plottedPiecewiseSplines = plotPartialPiecewiseSpline(uiData,...
+                                                              Pn,...
                                                               y_ONE,...
                                                               y_TWO,...
                                                               y_THREE,...
@@ -296,7 +301,8 @@ function plottedPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
         scatteredPointHandleToDelete = {};
     end
 
-    [newPointLabelHandles, newScatteredPointHandle] = plotPointsAndLabels(Pn,... 
+    [newPointLabelHandles, newScatteredPointHandle] = plotPointsAndLabels(uiData,...
+                                                                          Pn,... 
                                                                           points_labels,...
                                                                           isAdditionalSpline,...
                                                                           pointLabelHandlesToDelete,...
@@ -311,11 +317,12 @@ function plottedPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
     end
 end
 
-function [newPointLabelHandles, newScatteredPointHandle] = plotPointsAndLabels(Pn,...
-                                                                                pointsLabelStrings,...
-                                                                                isAdditionalSpline,...
-                                                                                pointsLabelHandles,...
-                                                                                scatteredPointsHandle)
+function [newPointLabelHandles, newScatteredPointHandle] = plotPointsAndLabels(uiData,...
+                                                                               Pn,...
+                                                                               pointsLabelStrings,...
+                                                                               isAdditionalSpline,...
+                                                                               pointsLabelHandles,...
+                                                                               scatteredPointsHandle)
     % isAdditionalSpline argument == 1 indicaten that the first point label of the
     % additional spline must be shifted in order to avoid overwritting the
     % last point initial sline label
@@ -324,12 +331,10 @@ function [newPointLabelHandles, newScatteredPointHandle] = plotPointsAndLabels(P
     % and newSscatteredPointHandle which hosts the handle returned by the scatter
     % function
 
-    global SCATTER_POINT_SIZE;
- 
     deleteScatteredPoints(scatteredPointsHandle);
     deletePointLabels(pointsLabelHandles);
     
-    newScatteredPointHandle = scatter(Pn(:,1),Pn(:,2),SCATTER_POINT_SIZE,'k','filled');
+    newScatteredPointHandle = scatter(Pn(:,1),Pn(:,2),uiData.SCATTER_POINT_SIZE,'k','filled');
     
     if isAdditionalSpline == 1
         % in this case, the point label is written in a shifted position
@@ -402,9 +407,8 @@ function yFuncCellArray = computeAdditionalPiecewiseSpline()
 %}
 end
 
-function plotAdditionalPiecewiseSpline(yFuncCellArray)
+function plotAdditionalPiecewiseSpline(yFuncCellArray, uiData)
     %adding new 4 points piecewise spline
-    global SCATTER_POINT_SIZE;
     global global_splines_data;
 
     Pn = [global_splines_data{1}(1,5:8)' global_splines_data{2}(1,5:8)'];
@@ -421,7 +425,8 @@ function plotAdditionalPiecewiseSpline(yFuncCellArray)
     
     isAdditionalSpline = 1;% first point label will be shifted to avoid overwritting
                            % last point label of initial piecewise spline
-    plottedAdditionalPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
+    plottedAdditionalPiecewiseSplines = plotPartialPiecewiseSpline(uiData,...
+                                                                   Pn,...
                                                                    y_D,...
                                                                    y_E,...
                                                                    y_F,...
@@ -434,12 +439,12 @@ end
 
 % ui callback functions
 
-function sliderPlot_x(hObject,event)
+function sliderPlot_x(hObject, event, uiData)
     global global_splines_data;
     
     n = get(hObject,'Value');
     
-    switch global_splines_data{3}
+    switch uiData.pointMenuCurrentSelection
         case 'P1'
             global_splines_data{1}(1,1) = n;
         case 'P2'
@@ -457,7 +462,7 @@ function sliderPlot_x(hObject,event)
         case 'P8'
             global_splines_data{1}(1,8) = n;
       otherwise
-            error('Invalid selection %s', global_splines_data{3});
+            error('Invalid selection %s', uiData.pointMenuCurrentSelection);
     end
     
     xSliderValueTextUI = global_splines_data{6};
@@ -477,7 +482,8 @@ function sliderPlot_x(hObject,event)
     deletePartialPiecewiseSpline(global_splines_data{13});
     isAdditionalSpline = 0; % first point label will be shifted to avoid overwritting
                             % last point label of initial piecewise spline
-    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
+    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(uiData,...
+                                                              Pn,...
                                                               y_A,...
                                                               y_B,...
                                                               y_C,...
@@ -500,7 +506,8 @@ function sliderPlot_x(hObject,event)
 
     isAdditionalSpline = 1;% first point label will be shifted to avoid overwritting
                            % last point label of initial piecewise spline
-    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(Pn,...
+    plottedFirstPiecewiseSplines = plotPartialPiecewiseSpline(uiData,...
+                                                              Pn,...
                                                               y_D,...
                                                               y_E,...
                                                               y_F,...
@@ -536,11 +543,11 @@ function deleteScatteredPoints(scatteredPointsHandle)
     end
 end
 
-function sliderPlot_y(hObject,event)
+function sliderPlot_y(hObject,event, uiData)
     global global_splines_data;
     n = get(hObject,'Value');
     
-    switch global_splines_data{3}
+    switch uiData.pointMenuCurrentSelection
         case 'P1'
             global_splines_data{2}(1,1) = n;
         case 'P2'
@@ -558,35 +565,35 @@ function sliderPlot_y(hObject,event)
         case 'P8'
             global_splines_data{2}(1,8) = n;
       otherwise
-            error('Invalid selection %s', global_splines_data{3})
+            error('Invalid selection %s', uiData.pointMenuCurrentSelection)
     end
     
     ySliderValueTextUI = global_splines_data{7};
     ySliderValueTextUI.String = n;
 end
 
-function updateSliderXProperties(slider, sliderTextValue, pointIndex)
-    global XY_SLIDER_STEP;
+function updateSliderXProperties(slider, sliderTextValue, pointIndex, uiData)
     global global_splines_data;
 
-    [xSliderMin xSliderMax] = getMinMaxX(pointIndex);
+    [xSliderMin xSliderMax] = getMinMaxX(pointIndex, uiData);
     slider.Max = xSliderMax;
     slider.Min = xSliderMin;
-    slider.SliderStep = [XY_SLIDER_STEP/(xSliderMax-xSliderMin), XY_SLIDER_STEP * 5/(xSliderMax-xSliderMin)];
+    slider.SliderStep = [uiData.XY_SLIDER_STEP/(xSliderMax-xSliderMin), uiData.XY_SLIDER_STEP * 5/(xSliderMax-xSliderMin)];
     xValue = global_splines_data{1}(1,pointIndex);
     slider.Value = xValue;
     sliderTextValue.String = xValue;
 end
 
-function menuSelection(hObject,event)
+function menuSelection(hObject, event)
     global global_splines_data;
+    global uiData;
     
     val = get(hObject,'Value');
     str = get(hObject,'String');
     
-    % saving menu selection into global_splines_data so the two slider
+    % saving menu selection into uiData so the two slider
     % callback functions can operate on the right point
-    global_splines_data{3} = str{val};
+    uiData.pointMenuCurrentSelection = str{val};
     
     % get slider and slider text references
     xSlider = global_splines_data{4};
@@ -594,56 +601,56 @@ function menuSelection(hObject,event)
     ySlider = global_splines_data{5};
     ySliderTextValue = global_splines_data{7};
 
-    switch global_splines_data{3}
+    switch uiData.pointMenuCurrentSelection
         case 'P1'
-            updateSliderXProperties(xSlider, xSliderTextValue, 1)
+            updateSliderXProperties(xSlider, xSliderTextValue, 1, uiData)
             
             yValue = global_splines_data{2}(1,1);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P2'
-            updateSliderXProperties(xSlider, xSliderTextValue, 2)
+            updateSliderXProperties(xSlider, xSliderTextValue, 2, uiData)
             
             yValue = global_splines_data{2}(1,2);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P3'
-            updateSliderXProperties(xSlider, xSliderTextValue, 3)
+            updateSliderXProperties(xSlider, xSliderTextValue, 3, uiData)
             
             yValue = global_splines_data{2}(1,3);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P4'
-            updateSliderXProperties(xSlider, xSliderTextValue, 4)
+            updateSliderXProperties(xSlider, xSliderTextValue, 4, uiData)
             
             yValue = global_splines_data{2}(1,4);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P5'
-            updateSliderXProperties(xSlider, xSliderTextValue, 5)
+            updateSliderXProperties(xSlider, xSliderTextValue, 5, uiData)
             
             yValue = global_splines_data{2}(1,5);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P6'
-            updateSliderXProperties(xSlider, xSliderTextValue, 6)
+            updateSliderXProperties(xSlider, xSliderTextValue, 6, uiData)
             
             yValue = global_splines_data{2}(1,6);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P7'
-            updateSliderXProperties(xSlider, xSliderTextValue, 7)
+            updateSliderXProperties(xSlider, xSliderTextValue, 7, uiData)
             
             yValue = global_splines_data{2}(1,7);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
         case 'P8'
-            updateSliderXProperties(xSlider, xSliderTextValue, 8)
+            updateSliderXProperties(xSlider, xSliderTextValue, 8, uiData)
             
             yValue = global_splines_data{2}(1,8);
             ySlider.Value = yValue;
             ySliderTextValue.String = yValue;
       otherwise
-            error('Invalid selection %s', global_splines_data{3});
+            error('Invalid selection %s', uiData.pointMenuCurrentSelection);
     end
 end
