7c7d0a9263e53cafe812bd9679fc0bcb80e8fac5
Archanciel <jp.schnyder@gmail.com>
2020-01-20 16:17:10 +0100

    init commit

---
+++ SplineData.m
@@ -0,0 +1,52 @@
+                           % {1} --> curve points x coordinates
+                           % {2} --> curve points y coordinates
+                           % {9} --> initial piecewise spline points_labels strings
+                           % {10} --> additional piecewise spline points_labels strings
+                           % {11} --> initial piecewise spline colors
+                           % {12} --> additional piecewise spline colors
+                           % {13} --> initial piecewise spline line handles
+                           % {14} --> additional piecewise spline line handles
+                           % {15} --> initial piecewise spline point label handles
+                           % {16} --> additional piecewise spline point label handles
+                           % {17} --> initial piecewise spline scattered point handles
+                           % {18} --> additional piecewise spline scattered point handles
+classdef SplineData < handle
+    % inheriting from handle in order for instances to be passed by reference
+    properties
+        % constants
+        SCATTER_POINT_SIZE = 15;
+        XY_SLIDER_STEP = 0.1;
+        
+        pointMenuCurrentSelection;
+        xSliderHandle;
+        ySliderHandle;
+        xSliderTextValueHandle;
+        ySliderTextValueHandle;
+    end
+    methods
+        function obj = UIControlsData()
+            obj.pointMenuCurrentSelection = 'P1';
+        end
+        
+        % using property setter (similar to python @property annotaton)
+        function obj = set.pointMenuCurrentSelection(obj, value)
+             obj.pointMenuCurrentSelection = value;
+        end
+        
+        function obj = set.xSliderHandle(obj, value)
+             obj.xSliderHandle = value;
+        end
+        
+        function obj = set.ySliderHandle(obj, value)
+             obj.ySliderHandle = value;
+        end
+        
+        function obj = set.xSliderTextValueHandle(obj, value)
+             obj.xSliderTextValueHandle = value;
+        end
+        
+        function obj = set.ySliderTextValueHandle(obj, value)
+             obj.ySliderTextValueHandle = value;
+        end
+    end
+end
