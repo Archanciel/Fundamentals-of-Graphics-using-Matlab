19a915c102f2e6475ea4a5cd08e9f99c15a65ba2
Archanciel <jp.schnyder@gmail.com>
2020-01-25 17:53:57 +0100

    Revert "Revert "init commit""
    This reverts commit 67fcb73fabe8fed60748975d8aee7f80b46b81eb.

--- SplineView.m
+++ SplineView.m
@@ -2,17 +2,22 @@ classdef SplineView < matlab.apps.AppBase
 % Source is chap1_8_adding_piecewise_spline_standard_form_app.mlapp
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure              matlab.ui.Figure
-        UIAxes                matlab.ui.control.UIAxes
-        Panel                 matlab.ui.container.Panel
-        XCoordSliderTxtValue  matlab.ui.control.Label
-        YCoordSliderTxtValue  matlab.ui.control.Label
-        XSliderLabel          matlab.ui.control.Label
-        XCoordSlider          matlab.ui.control.Slider
-        YSliderLabel          matlab.ui.control.Label
-        YCoordSlider          matlab.ui.control.Slider
-        PointDropDownLabel    matlab.ui.control.Label
-        PointSelectionMenu    matlab.ui.control.DropDown
+        % UI controls
+        UIFigure              matlab.ui.Figure;
+        UIAxes                matlab.ui.control.UIAxes;
+        Panel                 matlab.ui.container.Panel;
+        XCoordSliderTxtValue  matlab.ui.control.Label;
+        YCoordSliderTxtValue  matlab.ui.control.Label;
+        XSliderLabel          matlab.ui.control.Label;
+        XCoordSlider          matlab.ui.control.Slider;
+        YSliderLabel          matlab.ui.control.Label;
+        YCoordSlider          matlab.ui.control.Slider;
+        PointDropDownLabel    matlab.ui.control.Label;
+        PointSelectionMenu    matlab.ui.control.DropDown;
+        
+        % other properties
+        splineModel           SplineModel;
+        splineController      SplineController;
     end
 
     % Callbacks that handle component events
@@ -113,8 +118,9 @@ classdef SplineView < matlab.apps.AppBase
     methods (Access = public)
 
         % Construct app
-        function app = SplineView
-
+        function app = SplineView(splineModel, splineController)
+            app.splineModel = splineModel
+            app.splineController = splineController
             % Create UIFigure and components
             createComponents(app)
 
