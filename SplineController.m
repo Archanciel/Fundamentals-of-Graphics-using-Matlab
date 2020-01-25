0ca1a8e55bd887a15b00ee75caac0a84a816025f
Archanciel <jp.schnyder@gmail.com>
2020-01-25 17:10:14 +0100

    init commit

---
+++ SplineController.m
@@ -0,0 +1,11 @@
+classdef SplineController < handle
+    % inheriting from handle in order for instances to be passed by reference
+    properties
+        % constants
+        % none !
+    end
+    methods
+        function obj = SplineController()
+        end
+    end
+end
